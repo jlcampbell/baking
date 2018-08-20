0f363f22cfa31feb6a3204fc041eed3616830130
jessica <Jess@Jessicas-MacBook-Pro.local>
2018-08-19 23:26:41 -0400

    implementing room

--- app/src/main/java/com/campbell/jess/baking_app/data/model/Ingredient.java
+++ app/src/main/java/com/campbell/jess/baking_app/data/model/Ingredient.java
@@ -4,21 +4,40 @@ package com.campbell.jess.baking_app.data.model;
  * Created by jlcampbell on 7/19/2018.
  */
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+@Entity(tableName = "ingredient_table")
 public class Ingredient {
 
+    //table columns
     @SerializedName("quantity")
     @Expose
     private Number quantity;
+
     @SerializedName("measure")
     @Expose
     private String measure;
+
+    @PrimaryKey
+    @NonNull
     @SerializedName("ingredient")
     @Expose
     private String ingredient;
 
+    //constructor
+    public Ingredient(@NonNull String ingredient, Number quantity, String measure) {
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+        this.measure = measure;
+    }
+
+
+    //getters and setters
     public Number getQuantity() {
         return quantity;
     }
--- app/src/main/java/com/campbell/jess/baking_app/data/model/Recipe.java
+++ app/src/main/java/com/campbell/jess/baking_app/data/model/Recipe.java
@@ -5,31 +5,50 @@ package com.campbell.jess.baking_app.data.model;
  */
 
 
+        import android.arch.persistence.room.Entity;
+        import android.arch.persistence.room.PrimaryKey;
+
         import java.util.List;
         import com.google.gson.annotations.Expose;
         import com.google.gson.annotations.SerializedName;
 
-
+@Entity(tableName = "recipe_table")
 public class Recipe {
-
+    @PrimaryKey
     @SerializedName("id")
     @Expose
     private int id;
+
     @SerializedName("name")
     @Expose
     private String name;
+
     @SerializedName("ingredients")
     @Expose
     private List<Ingredient> ingredients = null;
+
     @SerializedName("steps")
     @Expose
     private List<Step> steps = null;
+
     @SerializedName("servings")
     @Expose
     private Integer servings;
+
     @SerializedName("image")
     @Expose
     private String image;
+
+
+    public Recipe(int id, String name, List<Ingredient> ingredients, List<Step> steps, int servings, String image){
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+
 
     public int getId() {
         return id;
